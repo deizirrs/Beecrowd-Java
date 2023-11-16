7e27e82c16917076b6cc2f79795a0ce6f3b2b058
deizirrs <deizianesantos.rodrigues15@gmail.com>
2023-11-16 14:22:58 -0300

    Esfera

---
+++ Be_1011.java
@@ -0,0 +1,19 @@
+package Exercicios_Beecrowd;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Be_1011 {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		Locale.setDefault(Locale.US);
+		
+		double raio = input.nextDouble();
+		double PI = 3.14159;
+		
+		double volEsfera = (4.0/3) * PI * Math.pow(raio, 3);
+System.out.printf("VOLUME = %.3f%n" , volEsfera);
+	}
+
+}
